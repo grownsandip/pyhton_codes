964482450ddfb2c28508b40fa89259f6e7d33798
Sandip <roysandip33290@gmail.com>
2024-09-17 18:30:06 +0530

    updated string operations

--- string_ops.py
+++ string_ops.py
@@ -1,6 +1,6 @@
 str="Hello world"
 print(len(str))
-print(str.size())
+#print(str.size())
 # t=["hi","there"]
 # print(str.lower())
 # print(str.upper())
@@ -9,4 +9,8 @@ print(str.size())
 # print(" ".join(t))
 # print(str.find("world"))
 # print(str.count("l"))
-print(str[::-2])
+print(str[0::-2])
+print(str.title()) #converts all first letter if of string capital
+print(str.startswith("Hello"))
+print(str.endswith("world"))
+print(str.istitle())
