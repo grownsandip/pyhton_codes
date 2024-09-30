fd6f0ce27eed21545c180c09b02c6d5093df32c3
Sandip <roysandip33290@gmail.com>
2024-09-30 10:13:20 +0530

    updated tuple

--- tuple.py
+++ tuple.py
@@ -1,4 +1,4 @@
-#tuples are immutable datatypes order collections of data items they can store multiple varibales in single
+# tuples are immutable datatypes order collections of data items they can store multiple varibales in single
 tup=(1,2,3,4,5,6,6,"string",True)
 print(tup,type(tup))
 print(tup[0])
@@ -7,13 +7,15 @@ print(tup[-2])
 if "string" in tup:
     print("yes it exists")
     
-#we can also do slicing in tuple which returns a new tuple
-# tup2=tup[1:5]
-# print(tup2)
-# print(tup.count(6)) #returns number of occurences
-# print(tup.index(6)) #returns the index of first occurance
+# we can also do slicing in tuple which returns a new tuple
+tup2=tup[1:5]
+print(tup2)
+print(tup.count(6)) #returns number of occurences
+print(tup.index(6)) #returns the index of first occurance
 # we can convert tuple into list and perform ll list operations on reconvert into tuple
-#we can concat two tuples
+# we can concat two tuples
 tup1=(1,2,3,4,5)
 tup2=(6,7,8,9,10)
 print(tup1+tup2)
+tup4=(1,) #declaring an empty tuple
+print(type(tup4))
